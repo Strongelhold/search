e8b9292c33ad5164227371d7889e1bd59b077656
strongelhold <strongelhold@gmail.com>
2016-03-16 23:43:30 +0300

    Add initial classes
    without logic

--- app/models/searcher.rb
+++ app/models/searcher.rb
@@ -1,2 +1,16 @@
 class Searcher < ActiveRecord::Base
+  def self.search(date, gds)
+    date = Date.parse(date)
+    if date <= Date.today
+      #обработка ошибок вывод сообщений
+      false
+    else
+      if gds.nil?
+        #обработка ошибок, вывод сообщений
+        false
+      else
+        
+      end
+    end
+  end
 end
